6b2cafbf4fca45343bbf3cd67b0d0abe8d1d0e13
MkYnhSn <41138303+MkYnhSn@users.noreply.github.com>
2018-07-30 15:07:50 +0800

    Add files via upload

--- java/com/bryancui/cbymod/proxy/ClientProxy.java
+++ java/com/bryancui/cbymod/proxy/ClientProxy.java
@@ -2,13 +2,21 @@ package com.bryancui.cbymod.proxy;
 
 import com.bryancui.cbymod.util.Reference;
 
+import net.minecraft.client.renderer.block.model.ModelBakery;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.block.statemap.StateMapperBase;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.ItemMeshDefinition;
+import net.minecraft.item.ItemStack;
+
 import net.minecraftforge.client.model.ModelLoader;
 
 public class ClientProxy extends CommonProxy{
 	
+	@Override
 	public void registerItemRenderer(Item item, int meta, String id){
 		
 		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), id));
@@ -20,5 +28,25 @@ public class ClientProxy extends CommonProxy{
 		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(new ResourceLocation(Reference.MOD_ID, filename), id));
 	}
 
-	
+	@Override
+	public void registerFluidModel(Item item, Block block, String name)
+	{
+		ModelBakery.registerItemVariants(item);
+		ModelLoader.setCustomMeshDefinition(item, new ItemMeshDefinition()
+		{
+			@Override
+			public ModelResourceLocation getModelLocation(ItemStack stack)
+			{
+				return new ModelResourceLocation(Reference.MOD_ID + ":" + name , name);
+			}
+		});
+		ModelLoader.setCustomStateMapper(block, new StateMapperBase()
+		{
+			@Override
+			protected ModelResourceLocation getModelResourceLocation(IBlockState state)
+			{
+				return new ModelResourceLocation(Reference.MOD_ID + ":" + name , name);
+			}
+		});
+	}
 }
--- java/com/bryancui/cbymod/proxy/CommonProxy.java
+++ java/com/bryancui/cbymod/proxy/CommonProxy.java
@@ -1,10 +1,11 @@
 package com.bryancui.cbymod.proxy;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 
 public class CommonProxy {
 	
 	public void registerItemRenderer(Item item, int meta, String id){}
 	public void registerVariantRenderer(Item item, int meta, String filename, String id){}
-
+	public void registerFluidModel(Item item, Block block, String name) {}
 }
